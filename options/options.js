a78eb835969d6f35b352589df3e1e629249ec6f5
Crowdbotics <team@crowdbotics.com>
2021-08-16 11:05:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "crimson_star_29847Identifier",
+  url: "https://crimson_star_29847Identifier.botics.co",
+  api: "https://crimson_star_29847Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
